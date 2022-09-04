81c6c1e13ac39d63f38556173749a0561fd884c8
anT0ny54 <109664822+anT0ny54@users.noreply.github.com>
2022-09-04 15:25:00 +0700

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -8,10 +8,10 @@ if [ -f /etc/mosdns/hosts ]
   then
     echo "Original hosts file found."
   else
-    cp /etc/hosts /etc/hosts.original
+    cp /etc/mosdns/hosts /etc/mosdns/hosts.original
 fi
 
-cp /etc/hosts.original /etc/hosts
+cp /etc/mosdns/hosts.original /etc/mosdns/hosts
 
 wget -O /etc/mosdns/hosts https://raw.githubusercontent.com/t0ny54/blocklistwithregex/main/export/blocklist.txt
 
