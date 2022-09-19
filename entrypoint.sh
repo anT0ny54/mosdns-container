d53d6c699d7b50603cc1ef3ac19940ac6b5e6958
anT0ny54 <109664822+anT0ny54@users.noreply.github.com>
2022-09-20 01:33:16 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-if [ ! -f /etc/mosdns/config.yaml ]; then
-	mkdir -p /etc/mosdns/
-	cp -u /config.yaml /etc/mosdns/config.yaml
-    	cp -u /geosite.dat /etc/mosdns/geosite.dat
-    	cp -u /geoip.dat /etc/mosdns/geoip.dat
-    	cp -u /hosts /etc/mosdns/hosts
-fi
+sh /etc/mosdns/install_hosts.sh
+
+sh /etc/mosdns/install_geodata.sh
 
 sed -i "s|PORT_PLACEHOLDER|${PORT}|;s|PATH_PLACEHOLDER|${DOH_PATH}|" /etc/mosdns/config.yaml
 
-/usr/bin/mosdns start --dir /etc/mosdns
+exec mosdns start -d /etc/mosdns
